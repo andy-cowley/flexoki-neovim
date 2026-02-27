392cab5d9291fae0bd485c61dc583ea825a4568e
ac <andy.m.cowley@googlemail.com>
2026-02-27 11:57:09 +0000

    More variety

--- lua/flexoki/highlights/treesitter.lua
+++ lua/flexoki/highlights/treesitter.lua
@@ -1,63 +1,67 @@
-local palette = require('flexoki.palette')
+local palette = require("flexoki.palette")
 
 local M = {}
 
 M.groups = function()
-	local c = palette.palette()
+  local c = palette.palette()
 
-	--- @type table<string, vim.api.keyset.highlight>
-	return {
-		['@text.literal']      = { link = 'Comment'      },
-		['@text.reference']    = { link = 'Identifier'   },
-		['@text.title']        = { link = 'Title'        },
-		['@text.uri']          = { link = 'Underlined'   },
-		['@text.underline']    = { link = 'Underlined'   },
-		['@text.todo']         = { link = 'Todo'         },
+  --- @type table<string, vim.api.keyset.highlight>
+  return {
+    ["@text.literal"] = { link = "Comment" },
+    ["@text.reference"] = { link = "Identifier" },
+    ["@text.title"] = { link = "Title" },
+    ["@text.uri"] = { link = "Underlined" },
+    ["@text.underline"] = { link = "Underlined" },
+    ["@text.todo"] = { link = "Todo" },
 
-		['@comment']           = { link = 'Comment'      },
-		['@punctuation']       = { link = 'Delimiter'    },
+    ["@comment"] = { link = "Comment" },
+    ["@punctuation"] = { link = "Delimiter" },
 
-		['@constant']          = { link = 'Constant'     },
-		['@constant.builtin']  = { link = 'Special'      },
-		['@constant.macro']    = { link = 'Define'       },
-		['@define']            = { link = 'Define'       },
-		['@macro']             = { link = 'Macro'        },
-		['@string']            = { link = 'String'       },
-		['@string.escape']     = { link = 'SpecialChar'  },
-		['@string.special']    = { link = 'SpecialChar'  },
-		['@character']         = { link = 'Character'    },
-		['@character.special'] = { link = 'SpecialChar'  },
-		['@number']            = { link = 'Number'       },
-		['@boolean']           = { link = 'Boolean'      },
-		['@float']             = { link = 'Float'        },
+    ["@constant"] = { link = "Constant" },
+    ["@constant.builtin"] = { link = "Special" },
+    ["@constant.macro"] = { link = "Define" },
+    ["@define"] = { link = "Define" },
+    ["@macro"] = { link = "Macro" },
+    ["@string"] = { link = "String" },
+    ["@string.escape"] = { link = "SpecialChar" },
+    ["@string.special"] = { link = "SpecialChar" },
+    ["@character"] = { link = "Character" },
+    ["@character.special"] = { link = "SpecialChar" },
+    ["@number"] = { link = "Number" },
+    ["@boolean"] = { link = "Boolean" },
+    ["@float"] = { link = "Float" },
 
-		['@function']          = { link = 'Function'     },
-		['@function.builtin']  = { link = 'Special'      },
-		['@function.macro']    = { link = 'Macro'        },
-		['@parameter']         = { link = 'Identifier'   },
-		['@method']            = { link = 'Function'     },
-		['@field']             = { link = 'Identifier'   },
-		['@property']          = { link = 'Identifier'   },
-		['@constructor']       = { link = 'Special'      },
+    ["@function"] = { link = "Function" },
+    ["@function.builtin"] = { link = "Special" },
+    ["@function.macro"] = { link = "Macro" },
+    ["@parameter"] = { link = "Identifier" },
+    ["@method"] = { link = "Function" },
+    ["@field"] = { link = "Identifier" },
+    ["@property"] = { link = "Identifier" },
+    ["@constructor"] = { link = "Special" },
 
-		['@conditional']       = { link = 'Conditional'  },
-		['@repeat']            = { link = 'Repeat'       },
-		['@label']             = { link = 'Label'        },
-		['@operator']          = { link = 'Operator'     },
-		['@keyword']           = { link = 'Keyword'      },
-		['@exception']         = { link = 'Exception'    },
+    ["@conditional"] = { link = "Conditional" },
+    ["@repeat"] = { link = "Repeat" },
+    ["@label"] = { link = "Label" },
+    ["@operator"] = { link = "Operator" },
+    ["@keyword"] = { link = "Keyword" },
+    ["@exception"] = { link = "Exception" },
 
-		['@variable']          = { link = 'Identifier'   },
-		['@type']              = { link = 'Type'         },
-		['@type.definition']   = { link = 'Typedef'      },
-		['@storageclass']      = { link = 'StorageClass' },
-		['@structure']         = { link = 'Structure'    },
-		['@namespace']         = { link = 'Identifier'   },
-		['@include']           = { link = 'Include'      },
-		['@preproc']           = { link = 'PreProc'      },
-		['@debug']             = { link = 'Debug'        },
-		['@tag']               = { link = 'Tag'          },
-	}
+    ["@variable"] = { link = "Identifier" },
+    ["@variable.builtin"] = { link = "Include" },
+    ["@variable.member"] = { link = "String" },
+    ["@variable.parameter"] = { link = "Include" },
+    ["@variable.parameter.builtin"] = { link = "Typedef" },
+    ["@type"] = { link = "Type" },
+    ["@type.definition"] = { link = "Typedef" },
+    ["@storageclass"] = { link = "StorageClass" },
+    ["@structure"] = { link = "Structure" },
+    ["@namespace"] = { link = "Identifier" },
+    ["@include"] = { link = "Include" },
+    ["@preproc"] = { link = "PreProc" },
+    ["@debug"] = { link = "Debug" },
+    ["@tag"] = { link = "Tag" },
+  }
 end
 
 return M
